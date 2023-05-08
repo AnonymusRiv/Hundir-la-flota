9ea96e1e0e73dc196bcaf0dcd0411d435a0ea4c9
Carlos Rivero <carlos10.arco@gmail.com>
2023-05-08 18:55:32 +0200

    BBDD conection succesful

--- App/user/serializer.py
+++ App/user/serializer.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
-from .models import Customer
+from .models import user
 
 class Userserializer(serializers.ModelSerializer):
 
     class Meta:
-        model = User
-        fields = ('pk', 'name', 'surname', 'email', 'description')
+        model = user
+        fields = ('id', 'name', 'surname', 'email', 'description')
--- App/user/views.py
+++ App/user/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import Userserializer
+from .models import user
 
 # Create your views here.
+class userview(viewsets.ModelViewSet):
+    serializer_class = Userserializer
+    queryset = user.objects.all()
